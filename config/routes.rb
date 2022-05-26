f7ac7fa9454e27434d4b22ff0058c97d8e02eabd
RobFernandes <robfernandes76@gmail.com>
2022-05-26 22:27:53 +0100

    updated bookmarks.rb, routes.rb and added API to seeds.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'lists#index'
-  resources :lists, only: [:index, :create, :new, :delete]
+  resources :lists, only: [:index, :create, :new, :show, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-puts "creating DB of movies"
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-puts "finised creating movies DB"
+require 'open-uri'
+require 'json'
+
+def add_movies_to_db(movies_list)
+  movies_list.each do |movie|
+    p movie
+    Movie.create(title: movie['title'], overview: movie['overview'], poster_url: "https://image.tmdb.org/t/p/original/#
+    {movie['poster_path']}", rating: movie ['vote_average'])
+  end
+end
+
+  url = "http://tmdb.lewagon.com/movie/top_rated?page="
+  page = 1
+  movie_request = JSON.parse(URI.open("#{url}#{page}").read)
+  add_movies_to_db(movie_request["results"])
+  # results for list of results. page for page number. total_pages = 438.
+  total_pages = 20
+  # total_pages = movie_request["total_pages]"]
+  while pages < total_pages
+    page += 1
+    puts "Fetching page: #{page}"
+    new_movie_request = JSON.parse(URI.open("#{url}#{page}").read)
+    add_movies_to_db(new_movie_request["results"])
+  end
